4d58778f0c0e2943745b1c0332afc39c51d9aa9b
SaberHardy <samirsamado5@gmail.com>
2022-02-18 19:36:58 +0300

    activate layers

--- justchat/settings.py
+++ justchat/settings.py
@@ -59,6 +59,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'justchat.wsgi.application'
 ASGI_APPLICATION = 'justchat.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
